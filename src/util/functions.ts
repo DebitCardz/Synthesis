16dcad530db8df98c96f9c4aeb3387b44c2d0687
Tech <49742865+DebitCardz@users.noreply.github.com>
2021-09-23 10:47:17 -0400

    refactor: removed legacy code (#19)
    If we need the config we can just get the client and get config from there.

--- src/util/functions.ts
+++ src/util/functions.ts
@@ -1,18 +1,8 @@
-import { CommandClient, CommandContext } from "../../deps.ts";
+import { CommandClient } from "../../deps.ts";
 import SynthesisClient from "../client.ts";
-import Config from "../types/Config.ts";
 
 export const trueEquals = (a: unknown, b: unknown): boolean =>
   (!a && !b) || (a === b);
-
-export const requireSynthesisConfigFromClient = (
-  client: CommandClient,
-): Config => {
-  return requireSynthesisClient(client).config;
-};
-
-export const requireSynthesisConfig = (ctx: CommandContext): Config =>
-  requireSynthesisConfigFromClient(ctx.client);
 
 export const requireSynthesisClient = (client: CommandClient) => {
   if (client instanceof SynthesisClient) {
